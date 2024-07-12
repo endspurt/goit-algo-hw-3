451ec4eeb8756a75408ee70a44e91c1477cae04d
endspurt <alemannien@gmail.com>
2024-07-12 21:00:10 +0200

    drei

--- Task_1.py
+++ Task_1.py
@@ -1,31 +1,49 @@
 import os
 import shutil
+import sys
 import argparse
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Copy and sort files by extension")
-    parser.add_argument('source_dir', type=str, help="Source directory path")
-    parser.add_argument('dest_dir', type=str, nargs='?', default='dist', help="Destination directory path (default is 'dist')")
+# Функція для парсингу аргументів командного рядка
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Рекурсивне копіювання файлів та сортування за розширенням.")
+    parser.add_argument('src', type=str, help='Шлях до вихідної директорії.')
+    parser.add_argument('dst', type=str, nargs='?', default='dist', help='Шлях до директорії призначення.')
     return parser.parse_args()
 
-def copy_files_recursively(source, dest):
-    if not os.path.exists(dest):
-        os.makedirs(dest)
+# Функція для рекурсивного копіювання файлів
+def copy_files(src, dst):
+    # Перевірка, чи існує директорія призначення, якщо ні - створюється
+    if not os.path.exists(dst):
+        os.makedirs(dst)
     
-    for root, dirs, files in os.walk(source):
-        for file in files:
-            file_path = os.path.join(root, file)
-            file_ext = os.path.splitext(file)[1][1:]
-            dest_dir = os.path.join(dest, file_ext)
+    # Перебір всіх елементів у вихідній директорії
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        # Якщо елемент є директорією, викликається функція рекурсивно
+        if os.path.isdir(s):
+            copy_files(s, dst)
+        else:
+            # Отримання розширення файлу
+            ext = item.split('.')[-1]
+            dest_dir = os.path.join(dst, ext)
+            # Перевірка, чи існує піддиректорія для цього розширення, якщо ні - створюється
             if not os.path.exists(dest_dir):
                 os.makedirs(dest_dir)
-            shutil.copy(file_path, dest_dir)
-        for dir in dirs:
-            copy_files_recursively(os.path.join(root, dir), dest)
+            # Копіювання файлу до відповідної піддиректорії
+            shutil.copy2(s, os.path.join(dest_dir, item))
 
+# Основна функція програми
 def main():
-    args = parse_args()
-    copy_files_recursively(args.source_dir, args.dest_dir)
+    args = parse_arguments()
+    try:
+        # Виклик функції копіювання файлів з отриманими аргументами
+        copy_files(args.src, args.dst)
+        print("Файли успішно скопійовані.")
+    except Exception as e:
+        # Виведення повідомлення про помилку у разі виникнення винятку
+        print(f"Помилка: {e}")
 
+# Виконання основної функції при запуску скрипта
 if __name__ == "__main__":
     main()
+
